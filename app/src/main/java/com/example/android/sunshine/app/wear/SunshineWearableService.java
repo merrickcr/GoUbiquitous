1cc84ac6524b8b0d4ff50da557cbe96c7b3e3b13
Christopher Merrick <christopher.merrick@capitalone.com>
2016-05-02 14:16:20 -0400

    no message

---
+++ app/src/main/java/com/example/android/sunshine/app/wear/SunshineWearableService.java
@@ -0,0 +1,8 @@
+package com.example.android.sunshine.app.wear;
+
+/**
+ * Created by atv684 on 4/19/16.
+ */
+public class SunshineWearableService {
+
+}
---
+++ sunshinewear/src/main/java/com/example/android/sunshine/app/SunshineWatchFaceService.java
@@ -0,0 +1,8 @@
+package com.example.android.sunshine.app;
+
+/**
+ * Created by atv684 on 4/19/16.
+ */
+public class SunshineWatchFaceService {
+
+}
